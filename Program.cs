3adcb07cef7dc0fed8a992eaabc5e098ec72ca21
Greg Ros <life.gregr@gmail.com>
2024-01-02 23:50:15 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Imms;
+Console.WriteLine("Hello, World!");
+var x = ImmList.Of(1, 2, 3);
+Console.WriteLine(String.Join(", ", x.AddLast(5)));
